581b4cf27038725089f13958cb35ff3e0c82c6cd
shariful707 <83864884+shariful707@users.noreply.github.com>
2024-02-17 00:11:03 +0600

    push

--- sql/jailmanage.sql
+++ sql/jailmanage.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 22, 2023 at 05:43 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: Feb 16, 2024 at 07:02 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,14 +31,14 @@ CREATE TABLE `admin` (
   `name` varchar(200) NOT NULL,
   `email` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`name`, `email`, `password`) VALUES
-('Walid Ibne Hasan', 'walid49@gmail.com', '93a5086d4a0f21b634c2518c249523247ebba14f0cabf5ae5bee54bfd5588556');
+('Syed Safwat Sameer', 'sameer@gmail.com', '93a5086d4a0f21b634c2518c249523247ebba14f0cabf5ae5bee54bfd5588556');
 
 -- --------------------------------------------------------
 
@@ -51,7 +51,7 @@ CREATE TABLE `request` (
   `shift` varchar(200) NOT NULL,
   `reason` varchar(1000) NOT NULL,
   `role` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -66,7 +66,7 @@ CREATE TABLE `schedule` (
   `shift` varchar(200) NOT NULL,
   `time` varchar(20) NOT NULL,
   `role` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `schedule`
@@ -91,20 +91,16 @@ CREATE TABLE `user` (
   `role` varchar(200) NOT NULL,
   `email` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `name`, `role`, `email`, `password`) VALUES
-(1, 'Kuddus Miah', 'Police', 'kuddus@gmail.com', '75f7f8c26cfe31a47113a7ce0b3493277cf392879e6fca1ebc7d93e597914e0c'),
-(5, 'Bilkis Begum', 'Chef', 'bilkis@gmail.com', 'bd514b20441bbbfe4edc22079e690736dcc888de2496c55963f9f5e2b43530f3'),
-(14, 'Safwat Samir', 'Police', 'samir@gmail.com', 'b816a16cd03774e0cefac03765680a33365d0b16060f67a2f7382a844f9c664f'),
-(22, 'Shuvo Talukders', 'Cleaner', 'shuvo@gmail.com', '2f684da2a727e1c49e48764b0c284c4835123ab7a330758066ed9b2b8721b810'),
-(29, 'Alif Hossain', 'Police', 'alif@gmail.com', '8c3fb8f94678f1214084c690601d0afd12a45c3b6940b040344c389b6225e547'),
-(30, 'Asif Khan', 'Cleaner', 'asif@gmail.com', '5f8b94103b61ea6cd6e258c67fd1cd52450d2e89d8f7c6afb5316742d5d9fe16'),
-(31, 'Tasnuva Haque', 'Chef', 'tasnuva@gmail.com', 'e6c3195a1a335f6d905544476838963273c1cb1ae4bb4c7033dc9f673268da6a');
+(32, 'Shariful Alam', 'Cleaner', 'sam@gmail.com', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f'),
+(33, 'Shafin Mufdi', 'Police', 'shafin@gmail.com', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f'),
+(35, 'Shadman Shakib', 'Chef', 'shakib@gmail.com', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f');
 
 --
 -- Indexes for dumped tables
@@ -142,7 +138,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
