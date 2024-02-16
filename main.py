dc9ab1ab32f0a6e03cd39edab65f7b59167024cf
ShafinMufdi <shafin.abrar.mufdi@g.bracu.ac.bd>
2024-02-16 22:58:37 +0600

    shafin mufdi

--- main.py
+++ main.py
@@ -200,3 +200,83 @@ def adminDash():
 ''')
         req = cursor.fetchall()
         return render_template('adminDash.html',user=user,req=req)
+
+
+
+@app.route('/staffDetails', methods = ['GET','POST'])
+def staffDetails():
+    info = 'staff'
+    if session['email'] != 'sameer@gmail.com':
+        return render_template('error.html')
+    else:
+        failed= ""
+        # Database connect
+        conn = MySQLdb.connect(host='localhost', user='root', passwd='', db='jailmanage')
+        cursor = conn.cursor()
+        # Query execute
+        cursor.execute('SELECT * FROM user')
+        # Matched row in 'user'
+        user = cursor.fetchall()
+
+        if request.method == 'POST':
+            if 'Add' == request.form.get('btn'):
+                name = request.form.get('name')
+                email = request.form.get('email')
+                password = request.form.get('password')
+                role = request.form.get('occupation')
+
+                existing_user = User.query.filter_by(email=email).first()
+                if existing_user:
+                    failed="Email already exists. Please choose a different email!"
+                else:
+                    #hashed password
+                    hashedPassword = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+                    entry = User(name=name, role=role, password=hashedPassword, email=email)
+                    db.session.add(entry)
+                    db.session.commit()
+
+                    # Database connect
+                    conn = MySQLdb.connect(host='localhost', user='root', passwd='', db='jailmanage')
+                    cursor = conn.cursor()
+                    # Query execute
+                    cursor.execute('SELECT * FROM user')
+                    # Matched row in 'user'
+                    user = cursor.fetchall()
+                    failed="Added!"
+
+            elif '🍳' == request.form.get('btn'):
+                email = request.form.get('email')
+                user = User.query.filter_by(email=email).first()
+                # Database connect
+                conn = MySQLdb.connect(host='localhost', user='root', passwd='', db='jailmanage')
+                cursor = conn.cursor()
+                # Query execute
+                cursor.execute('SELECT * FROM user WHERE email = %s', (email,))
+                # Matched row in 'user'
+                user = cursor.fetchall()
+                    
+            else:
+                email = request.form.get('email')
+                user = User.query.filter_by(email=email).first()
+                if user:
+                    db.session.delete(user)
+                    db.session.commit()
+                    user = Schedule.query.filter_by(email=email).first()
+                    if user:
+                        db.session.delete(user)
+                        db.session.commit()
+                # Database connect
+                conn = MySQLdb.connect(host='localhost', user='root', passwd='', db='jailmanage')
+                cursor = conn.cursor()
+                # Query execute
+                cursor.execute('SELECT * FROM user')
+                # Matched row in 'user'
+                user = cursor.fetchall()
+                # failed="Deleted!"
+
+        return render_template('staffDetails.html',user=user,failed=failed,info=info)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
